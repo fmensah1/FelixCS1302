ce7a8bfd88cb8fdb7284e7eda083dcff72513bee
fmensah1 <fmensah1@my.westga.edu>
2024-11-20 13:56:17 -0500

    Added menu to the UI
    Added a menu bar with a File menu that has the Menu Items About and Close.

--- CODE/Lab 9/src/edu/westga/cs1302/password_generator/view/MainWindow.java
+++ CODE/Lab 9/src/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -2,11 +2,15 @@ package edu.westga.cs1302.password_generator.view;
 
 import edu.westga.cs1302.password_generator.viewmodel.ViewModel;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /** Codebehind for the MainWindow of the Application.
  * 
@@ -14,7 +18,9 @@ import javafx.scene.control.TextField;
  * @version Fall 2024
  */
 public class MainWindow {
-
+	@FXML private AnchorPane guiPane;
+	@FXML private MenuItem aboutMenuItem;
+	@FXML private MenuItem closeMenuItem;
     @FXML private CheckBox mustIncludeDigits;
     @FXML private CheckBox mustIncludeLowerCaseLetters;
     @FXML private CheckBox mustIncludeUpperCaseLetters;
@@ -36,6 +42,22 @@ public class MainWindow {
     	
     	this.output.textProperty().bind(this.vm.getPassword());
     	this.errorTextLabel.textProperty().bind(this.vm.getErrorText());
+    	
+    	this.closeMenuItem.setOnAction((event) -> {
+    		Stage stage = (Stage) (this.guiPane).getScene().getWindow();
+    		stage.close();
+    	});
+    	
+    	this.aboutMenuItem.setOnAction((event)-> { 
+    		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    		alert.setTitle("About");
+    		alert.setHeaderText("Password Generator");
+    		alert.setContentText("This programis designed to generate a "
+    				+ "password based on mininum lenth, whether or not the password shoud have "
+    				+ "a lower case, upper case or digit."
+    				+ "\n Author: Felix Mensah");
+    		alert.showAndWait();
+    	});
     	
     	this.generatePasswordButton.setOnAction(
     			(event) -> { 
