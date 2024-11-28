8b99620161db67d62dce7be8c3e720e46fd485ec
fmensah1 <fmensah1@my.westga.edu>
2024-11-28 01:16:51 -0500

    Updated UI to save tasks
    Updated UI and main window to support saving tasks unto a selected file

--- CODE/Project 3/src/edu/westga/cs1302/project3/view/MainWindow.java
+++ CODE/Project 3/src/edu/westga/cs1302/project3/view/MainWindow.java
@@ -22,6 +22,7 @@ public class MainWindow {
 
 	@FXML private ListView<Task> taskListView;
     @FXML private MenuItem loadMenuItem;
+    @FXML private MenuItem saveMenuItem;
 	
     private ViewModel vm;
     private TaskManager taskManager;
@@ -32,28 +33,57 @@ public class MainWindow {
 		this.taskManager = new TaskManager();
 		this.taskListView.setItems(this.vm.getTaskList());
 		this.loadMenuItem.setOnAction((event)-> {
+			this.loadTask();
+		});
+		this.saveMenuItem.setOnAction((event)-> {
 			FileChooser fileChooser = new FileChooser();
-			fileChooser.setTitle("Open task file");
+			fileChooser.setTitle("Choose task file");
 			fileChooser.getExtensionFilters().addAll(
 					new ExtensionFilter("Text files", "*.txt", "*.pdf"),
-					new ExtensionFilter("All Files", "*.*"));
-		File selectedFile = fileChooser.showOpenDialog(null);
-		if (selectedFile != null) {
-			try {
-	            TaskList loadedTasks = this.taskManager.loadTasks(selectedFile.getAbsolutePath());
-	            this.vm.setTaskList(loadedTasks.getTasks()); 
-	        } catch (IllegalArgumentException error) {
-	            Alert alert = new Alert(Alert.AlertType.ERROR);
-	            alert.setContentText("Invalid file format: " + error.getMessage());
-	            alert.show();
-	        } catch (Exception error) {
-	            Alert alert = new Alert(Alert.AlertType.ERROR);
-	            alert.setContentText("Failed to load tasks: " + error.getMessage());
-	            alert.show();
-	        }
-		}
+					new ExtensionFilter("All files", "*."));
+			File selectedFile = fileChooser.showOpenDialog(null);
+			if (selectedFile != null) {
+				try {
+					TaskList taskList = new TaskList();
+					for (Task task : this.taskListView.getItems()) {
+		                taskList.addTask(task);
+		            }
+					this.taskManager.saveTaskList(taskList, selectedFile.getAbsolutePath());
+				}	 catch (IllegalArgumentException error) {
+				    Alert alert = new Alert(Alert.AlertType.ERROR);
+				    alert.setContentText("File cannot be overwritten: " + error.getMessage());
+				    alert.show();
+				} catch (Exception error) {
+				    Alert alert = new Alert(Alert.AlertType.ERROR);
+				    alert.setContentText("Failed to save tasks: " + error.getMessage());
+				    alert.show();
+				}
+			}
 			
 		});
+	}
+
+	private void loadTask() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open task file");
+		fileChooser.getExtensionFilters().addAll(
+				new ExtensionFilter("Text files", "*.txt", "*.pdf"),
+				new ExtensionFilter("All Files", "*.*"));
+File selectedFile = fileChooser.showOpenDialog(null);
+if (selectedFile != null) {
+		try {
+		    TaskList loadedTasks = this.taskManager.loadTasks(selectedFile.getAbsolutePath());
+		    this.vm.setTaskList(loadedTasks.getTasks()); 
+		} catch (IllegalArgumentException error) {
+		    Alert alert = new Alert(Alert.AlertType.ERROR);
+		    alert.setContentText("Invalid file format: " + error.getMessage());
+		    alert.show();
+		} catch (Exception error) {
+		    Alert alert = new Alert(Alert.AlertType.ERROR);
+		    alert.setContentText("Failed to load tasks: " + error.getMessage());
+		    alert.show();
+		}
+}
 	}
 
 }
